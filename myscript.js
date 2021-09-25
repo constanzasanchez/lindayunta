2217334213558a867b93e1355c9a00a24f19e978
cocosan <constanzasanchez1406@gmail.com>
2021-09-24 22:00:29 -0300

    agrego arrays y metodos

--- myscript.js
+++ myscript.js
@@ -1,3 +1,9 @@
+function saludo() {
+    let nombre = prompt('Escribe tu nombre');
+    alert('Bienvenido/a'+ nombre);
+}
+saludo();
+
 class Productos {
     constructor(nombre, precio){
         this.nombre = nombre.toLowerCase();
@@ -7,24 +13,30 @@ class Productos {
     sumaIva() {
         this.precio = this.precio * 1.21;
     }
-
     vender() {
         this.vendido = true;
     }
 }
-const palito = new Productos (palito, 1500);
-const helado = new Productos (helado, 1500);
-const bombito = new Productos (bombito, 1600);
-const caramelito = new Productos (caramelito, 1000);
-const mentita = new Productos (mentita, 2000);
-const cubik = new Productos (cubik, 1600);
-const bombe = new Productos (bombe, 1500);
-const dados = new Productos (dados, 1000);
-palito.sumaIva();
-helado.sumaIva();
-bombito.sumaIva();
-caramelito.sumaIva();
-mentita.sumaIva();
-cubik.sumaIva();
-bombe.sumaIva();
-dados.sumaIva();
+let allProducts = [];
+
+allProducts.push(new Productos ('heladito', 1500));
+allProducts.push(new Productos ('bombito', 1600));
+allProducts.push(new Productos ('palito', 1500));
+allProducts.push(new Productos ('boomerang', 1200));
+allProducts.push(new Productos ('cubik', 1600));
+allProducts.push(new Productos ('dados', 1000));
+allProducts.push(new Productos ('caramelito', 1000));
+allProducts.push(new Productos ('bom', 2000));
+
+console.log(allProducts);
+
+allProducts.forEach((producto) => {
+    producto.sumaIva();
+    console.log(producto);    
+});
+
+const busquedaProducto = allProducts.find ((producto)=> producto.nombre === 'palito');
+console.log(busquedaProducto);
+
+const busquedaPrecio = allProducts.filter ((producto) => producto.precio <= 1500);
+console.log(busquedaPrecio);
