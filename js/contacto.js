ba1533b678e25f9e7e90e0a11624b8ce4553b10c
cocosan <constanzasanchez1406@gmail.com>
2021-10-06 20:23:36 -0300

    eventos en seccion contacto

--- js/contacto.js
+++ js/contacto.js
@@ -1,29 +1,39 @@
+//Selectores
+    let nombreUsuario = document.getElementById("nombre");
+    let apellidoUsuario = document.getElementById("apellido");
+    let emailUsuario = document.getElementById("email");
+    let mensajeUsuario = document.getElementById('msj');
+
+//Guardo en consola los datos del usuario ingresado y una vez que
+//se vuelve a cargar la pagina se reinician los datos
+
 const formulario = document.getElementById("formulario");
 formulario.addEventListener("submit", mostrarFormulario);
+
+let valorNombreUsuario = nombreUsuario.value;
+let valorApellidoUsuario = apellidoUsuario.value;
+let valorEmailUsuario = emailUsuario.value;
+let valorMensajeUsuario = mensajeUsuario.value;
 
 function mostrarFormulario(e) {
     e.preventDefault();
     console.log("Formulario Enviado");
-    let nombreUsuario = document.getElementById("nombre").value;
-    let apellidoUsuario = document.getElementById("apellido").value;
-    let emailUsuario = document.getElementById("apellido").value;
 
-    console.log(nombre);
-    console.log(numero);
+    console.log(valorNombreUsuario);
+    console.log(valorApellidoUsuario);
+    console.log(valorEmailUsuario);
+    console.log(valorMensajeUsuario);
     
     e.target.reset();
 }
 
-//const nombreUsuario = document.getElementById('nombre');
-//const apellidoUsuario = document.getElementById('apellido');
-//const emailUsuario = document.getElementById('email');
-//const mensajeUsuario = document.getElementById('msj');
+//Agrego un color gris cuando se completa el campo 
 
-//nombreUsuario.addEventListener("focusin", () => {
-//    nombreUsuario.style.backgroundColor = "#E3E4E5"});
-//apellidoUsuario.addEventListener("focusin",() => {
-//    apellidoUsuario.style.backgroundColor = "#E3E4E5"});
-
-//nombreUsuario.addEventListener('blur',validarTexto);
-//document.getElementById('email').addEventListener('blur',validarEmail);
-//document.getElementById('numero').addEventListener('blur',validarNumero);
+nombreUsuario.addEventListener("focusin", () => {
+    nombreUsuario.style.backgroundColor = "#E3E4E5"});
+apellidoUsuario.addEventListener("focusin",() => {
+    apellidoUsuario.style.backgroundColor = "#E3E4E5"});
+emailUsuario.addEventListener("focusin",() => {
+    emailUsuario.style.backgroundColor = "#E3E4E5"});
+mensajeUsuario.addEventListener("focusin",() => {
+    mensajeUsuario.style.backgroundColor = "#E3E4E5"});
