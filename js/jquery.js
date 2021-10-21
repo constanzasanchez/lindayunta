1ad85572ad6b535ade4bbffd91121ab2486a5f6d
cocosan <constanzasanchez1406@gmail.com>
2021-10-20 22:51:31 -0300

    animaciones

--- js/jquery.js
+++ js/jquery.js
@@ -7,4 +7,12 @@ $("#textoFade").fadeOut("slow", function(){
     $("#textoFade").fadeIn(2000);
 }); 
 
-//$('.left-img').fadeOut(2000);
+$("#imgAbout").fadeIn(3000);
+
+$("#textoanimado1").css("margin-top", "40px")
+.slideUp(2000)
+.slideDown(2000);
+$("#textoanimado2")
+.css("margin-top", "40px")
+.slideUp(2000)
+.slideDown(2000);
