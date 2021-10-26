2ecf656caec1eb3e92b2011ad91adf04e82cf904
cocosan <constanzasanchez1406@gmail.com>
2021-10-26 12:26:41 -0300

    loader

--- js/typed.js
+++ js/typed.js
@@ -5,9 +5,9 @@ const typed = new Typed('.typed', {
 		'<i class="palabra">Cargando...</i>',
 	],
 	stringsElement: '#cadenas-texto', 
-	typeSpeed: 60, // Velocidad en mlisegundos para poner una letra,
-	startDelay: 0, // Tiempo de retraso para iniciar la animacion
-	backSpeed: 60, // Velocidad en milisegundos para borrrar una letra,
+	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 200, // Tiempo de retraso para iniciar la animacion
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
 	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
 	shuffle: false, // Alterar el orden en el que escribe las palabras.
 	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
